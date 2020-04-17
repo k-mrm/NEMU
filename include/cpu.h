55a208ee1134d8f429dc8083ac80e8d5ec7c519d
mrm <kei.id0305@gmail.com>
2020-04-18 03:24:53 +0900

    add cpu

---
+++ include/cpu.h
@@ -0,0 +1,28 @@
+#ifndef NEMU_CPU_H
+#define NEMU_CPU_H
+
+typedef struct CPU CPU;
+
+enum {
+    P_STATUS_CARRY      = 0b00000001,
+    P_STATUS_ZERO       = 0b00000010,
+    P_STATUS_IRQ        = 0b00000100,
+    P_STATUS_DECIMAL    = 0b00001000,
+    P_STATUS_BRK        = 0b00010000,
+    P_STATUS_RESERVED   = 0b00100000,
+    P_STATUS_OVERFLOW   = 0b01000000,
+    P_STATUS_NEGATIVE   = 0b10000000,
+}
+
+struct CPU {
+    unsigned char a;
+    unsigned char x;
+    unsigned char y;
+    unsigned short pc;
+    unsigned char sp;
+    unsigned char p;
+};
+
+extern CPU cpu;
+
+#endif
---
+++ src/cpu.c
@@ -0,0 +1,7 @@
+#include "cpu.h"
+
+CPU cpu;
+
+void init_cpu() {
+    ;
+}
