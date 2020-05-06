5a4d1196f65e58ba8b0eef611edc75ef39594537
mrm <kei.id0305@gmail.com>
2020-05-07 03:40:53 +0900

    add sprite

---
+++ include/ppu/sprite.h
@@ -0,0 +1,14 @@
+#ifndef NEMU_PPU_SPRITE_H
+#define NEMU_PPU_SPRITE_H
+
+#include <stdint.h>
+
+typedef struct Sprite Sprite;
+struct Sprite {
+    uint8_t y;
+    uint8_t tile;
+    uint8_t attr;
+    uint8_t x;
+};
+
+#endif
