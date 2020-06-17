d80c7544e87b95ad688394a96773dbd6bb411968
mrm <kei.id0305@gmail.com>
2020-06-18 02:44:41 +0900

    refactor

--- src/ppu/tile.c
+++ src/ppu/tile.c
@@ -26,35 +26,16 @@ static void ppu_make_pixelpat(PPU *ppu, Tile *tile, uint16_t sid, uint8_t vhflip
 
   memset(tile, 0, sizeof(Tile));
 
-  switch(vhflip) {
-    case 0:
-      for(int i = 0; i < 16; ++i) {
-        for(int j = 0; j < 8; ++j) {
-          tile->pp[i % 8][j] |= ((spr[i] & (0x80 >> j)) != 0) << (i / 8);
-        }
-      }
-      break;
-    case 1: /* hflip */
-      for(int i = 0; i < 16; ++i) {
-        for(int j = 0; j < 8; ++j) {
-          tile->pp[i % 8][7 - j] |= ((spr[i] & (0x80 >> j)) != 0) << (i / 8);
-        }
-      }
-      break;
-    case 2: /* vflip */
-      for(int i = 0; i < 16; ++i) {
-        for(int j = 0; j < 8; ++j) {
-          tile->pp[7 - i % 8][j] |= ((spr[i] & (0x80 >> j)) != 0) << (i / 8);
-        }
-      }
-      break;
-    case 3:
-      for(int i = 0; i < 16; ++i) {
-        for(int j = 0; j < 8; ++j) {
-          tile->pp[7 - i % 8][7 - j] |= ((spr[i] & (0x80 >> j)) != 0) << (i / 8);
-        }
-      }
-      break;
+  uint8_t hflip = vhflip & 1;
+  uint8_t vflip = vhflip & 2;
+
+  for(int i = 0; i < 16; ++i) {
+    for(int j = 0; j < 8; ++j) {
+      uint8_t offset_i = vflip? 7 - i % 8 : i % 8;
+      uint8_t offset_j = hflip? 7 - j : j; 
+
+      tile->pp[offset_i][offset_j] |= ((spr[i] & (0x80 >> j)) != 0) << (i / 8);
+    }
   }
 }
 
